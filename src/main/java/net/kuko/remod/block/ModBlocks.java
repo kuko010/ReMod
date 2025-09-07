e0ca578f7cfcfed97314b8f822db7bb7794370a7
kuko010 <kukouwu@proton.me>
2025-09-07 15:38:14 +0200

    Removed "PEDESTAL" Block

---
+++ src/main/java/net/kuko/remod/block/ModBlocks.java
@@ -0,0 +1,40 @@
+package net.kuko.remod.block;
+
+import net.kuko.remod.ReMod;
+import net.kuko.remod.item.ModItems;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredBlock;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+import java.util.function.Supplier;
+
+public class ModBlocks {
+    public static final DeferredRegister.Blocks BLOCKS = DeferredRegister.createBlocks(ReMod.MOD_ID);
+
+
+
+
+//    public static final DeferredBlock<Block> PEDESTAL = registerBlock("pedestal",
+//            () -> new PedestalBlock(BlockBehaviour.Properties.ofFullCopy(Blocks.STONE)));
+
+
+
+    private static <T extends Block> DeferredBlock<T> registerBlock(String name, Supplier<T> block) {
+        DeferredBlock<T> toReturn = BLOCKS.register(name, block);
+        registerBlockItem(name, toReturn);
+        return toReturn;
+    }
+
+    private static <T extends Block> void registerBlockItem(String name, DeferredBlock<T> block) {
+        ModItems.ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties()));
+    }
+
+    public void registerModBlocks(IEventBus bus) {
+        BLOCKS.register(bus);
+    }
+}
